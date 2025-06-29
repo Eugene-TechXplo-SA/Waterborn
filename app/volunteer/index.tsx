a1376776760f9e4f86edf5133ae83027144ad996
Eugene-TechXplo-SA <eugene@techxplo.co.za>
2025-06-29 16:00:35 +0200

    Updated index.tsx

--- app/volunteer/index.tsx
+++ app/volunteer/index.tsx
@@ -376,6 +376,7 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
     flex: 1,
     marginLeft: spacing.sm,
+    borderRadius: 5,
   },
   registerGradient: {
     flexDirection: 'row',
