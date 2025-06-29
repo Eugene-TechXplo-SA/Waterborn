0710a76a923878f486aa4fd7d97e98be4340be94
Eugene-TechXplo-SA <eugene@techxplo.co.za>
2025-06-29 15:30:13 +0200

    Updated CustomHeader.tsx

--- components/ui/CustomHeader.tsx
+++ components/ui/CustomHeader.tsx
@@ -19,7 +19,7 @@ export default function CustomHeader({ title, showBack = true, rightComponent }:
         colors={["#2563eb", "#06b6d4"]}
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 0 }}
-        style={{ paddingTop: 55, paddingBottom: 14, marginTop: -50, marginBottom: 15 }}
+        style={{ paddingTop: 55, paddingBottom: 14, marginTop: -50, marginBottom: 25 }}
         className="shadow-2xl relative"
       >
        
