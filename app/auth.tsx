9aad449536ff8534ebca188aac126e8e7f1a070b
Eugene-TechXplo-SA <eugene@techxplo.co.za>
2025-06-29 15:21:50 +0200

    Updated auth.tsx

--- app/auth.tsx
+++ app/auth.tsx
@@ -180,7 +180,7 @@ const styles = StyleSheet.create({
     borderRadius: borderRadius.lg,
   } as ViewStyle,
   activeTabGradient: {
-    paddingVertical: spacing.sm,
+    paddingVertical: 10,
     paddingHorizontal: spacing.md,
     borderRadius: borderRadius.lg,
     alignItems: 'center',
