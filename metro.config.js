3d3aac471c0d9690504651faa5a00fb8cd19aebf
Eugene-TechXplo-SA <eugene@techxplo.co.za>
2025-06-30 18:43:50 +0200

    Fix Metro config for import.meta support

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,7 @@ const { withNativeWind } = require("nativewind/metro");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Add support for .mjs files to resolve import.meta syntax errors
+config.resolver.sourceExts.push("mjs");
+
 module.exports = withNativeWind(config, { input: "./global.css" }); 
