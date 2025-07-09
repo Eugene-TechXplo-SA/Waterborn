af32c9f1945c556e2f4e555c8b005d3ca5cb1b9c
Eugene-TechXplo-SA <eugene@techxplo.co.za>
2025-07-09 21:23:58 +0200

    Fix import.meta syntax error

--- babel.config.js
+++ babel.config.js
@@ -3,13 +3,11 @@ module.exports = function (api) {
   return {
     presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
     plugins: [
-      ["babel-plugin-transform-import-meta", { "module": "ES6" }],
       "@babel/plugin-syntax-import-meta"
     ],
     env: {
       web: {
         plugins: [
-          ["babel-plugin-transform-import-meta", { "module": "ES6" }],
           "@babel/plugin-syntax-import-meta"
         ]
       }
