a0462ce3fc505a90df48b5554e9636256d4646cf
Eugene-TechXplo-SA <eugene@techxplo.co.za>
2025-07-09 21:22:57 +0200

    Fix import.meta syntax error in babel config

--- babel.config.js
+++ babel.config.js
@@ -2,12 +2,15 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
-    plugins: ["@babel/plugin-syntax-import-meta", "babel-plugin-transform-import-meta"],
+    plugins: [
+      ["babel-plugin-transform-import-meta", { "module": "ES6" }],
+      "@babel/plugin-syntax-import-meta"
+    ],
     env: {
       web: {
         plugins: [
-          "@babel/plugin-syntax-import-meta",
-          "babel-plugin-transform-import-meta"
+          ["babel-plugin-transform-import-meta", { "module": "ES6" }],
+          "@babel/plugin-syntax-import-meta"
         ]
       }
     }
