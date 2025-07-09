4f51877843b6b88b00132bd398fe7cfff02a1e5d
Eugene-TechXplo-SA <eugene@techxplo.co.za>
2025-07-09 21:21:26 +0200

    Updated babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,12 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
-    plugins: ["@babel/plugin-syntax-import-meta"],
+    plugins: ["@babel/plugin-syntax-import-meta", "babel-plugin-transform-import-meta"],
     env: {
       web: {
         plugins: [
-          "@babel/plugin-syntax-import-meta"
+          "@babel/plugin-syntax-import-meta",
+          "babel-plugin-transform-import-meta"
         ]
       }
     }
