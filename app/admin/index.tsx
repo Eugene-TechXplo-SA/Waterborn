7e3427e8a4d057c706d4a1fac0c55e3b1d62e2c4
Eugene-TechXplo-SA <eugene@techxplo.co.za>
2025-08-06 22:55:10 +0200

    Updated index.tsx

--- app/admin/index.tsx
+++ app/admin/index.tsx
@@ -357,7 +357,7 @@ const styles = StyleSheet.create({
     color: colors.text.secondary,
   },
   permissionsButton: {
-    borderRadius: borderRadius.lg,
+    borderRadius: borderRadius.m,
     overflow: 'hidden',
     alignSelf: 'center',
     width: '80%',
